d8b8b43c5521f81991c55d24c3d787ef5735f413
Bruno Bulado <101830204+brunobulado@users.noreply.github.com>
2023-12-11 16:47:18 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -150,7 +150,7 @@ const gameOver = () => {
     menu.style.display = "flex"
     finalScore.innerText = score.innerText
     canvas.style.filter = "blur(2px)"
-    gamescr.classList.add = "span.game-score"
+    gamescr.classList.add("span.game-score")
 
 }
 
